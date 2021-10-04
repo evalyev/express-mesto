0cc7d3e56e873eeffad60546a593878fb923225d
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-10-04 17:09:38 +0300

    fix: Исправлены ошибки в routes/cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -9,26 +9,26 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
-  }).unknown(true),
+    link: Joi.string().required().pattern(/https?:\/\/w*\.?[a-z\-._~:/?#[\]@!$&'()*+,;=]{0,}/i),
+  }),
 }), createCard);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string(),
-  }).unknown(true),
+    cardId: Joi.string().length(24).hex(),
+  }),
 }), removeCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string(),
-  }).unknown(true),
+    cardId: Joi.string().length(24).hex(),
+  }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string(),
-  }).unknown(true),
+    cardId: Joi.string().length(24).hex(),
+  }),
 }), dislikeCard);
 
 module.exports = router;
