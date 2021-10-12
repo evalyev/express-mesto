cddaf281274a5651490c17715e35b0fd2b4321d1
Evgeny Bakaev <bak.evsha@yandex.ru>
2021-10-13 00:41:42 +0300

    feat: cors added

--- app.js
+++ app.js
@@ -31,14 +31,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(function (req, res, next) {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых 
-  // if (allowedCors.includes(origin)) {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true)
   }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
   }
 
   next();
