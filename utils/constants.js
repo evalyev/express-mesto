824e68d7068d50b3e56a37b027d02a28314decef
Evgeny Bakaev <bak.evsha@yandex.ru>
2021-10-13 01:56:34 +0300

    feat: added set-cookie in allowedHeaders

--- utils/constants.js
+++ utils/constants.js
@@ -18,7 +18,7 @@ const options = {
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept'],
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Accept', 'Set-Cookie'],
   credentials: true,
 };
 
