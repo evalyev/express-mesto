805b99f33443d0541c3bca13b0ee3bbf46d64408
Evgeny Bakaev <bak.evsha@yandex.ru>
2021-10-14 19:44:03 +0300

    авторизация через локальное хранилище

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,42 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-error');
-const checkError = require('./check-errors');
+// const checkError = require('./check-errors');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.jwt;
 
-  if (!token) {
-    return checkError(new AuthError('Error. You need to log in'), req, res, next);
+//   if (!token) {
+//     return checkError(new AuthError('Error. You need to log in'), req, res, next);
+//   }
+
+//   let payload;
+
+//   try {
+//     // попытаемся верифицировать токен
+//     // eslint-disable-next-line no-undef
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+//   } catch (err) {
+//     // отправим ошибку, если не получилось
+//     return checkError(new AuthError('Error. You need to log in'), req, res, next);
+//   }
+
+//   req.user = payload;
+//   next();
+// };
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  // убеждаемся, что он есть или начинается с Bearer
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Error. You need to log in'));
   }
+  // извлечём токен
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
@@ -20,7 +46,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     // отправим ошибку, если не получилось
-    return checkError(new AuthError('Error. You need to log in'), req, res, next);
+    return next(new AuthError('Error. You need to log in'));
   }
 
   req.user = payload;
