c3717554a39a0babb398e805cd3de19d667854bb
Evgeny Bakaev <bak.evsha@yandex.ru>
2021-10-14 19:57:04 +0300

    feat: authoriza

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -30,6 +30,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  res.req({ authorization });
 
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
