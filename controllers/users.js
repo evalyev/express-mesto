f31294597507ac21edc62a1c31b28519bfbffbe1
Evgeny Bakaev <bak.evsha@yandex.ru>
2021-10-14 19:47:27 +0300

    feat: token in local storage

--- controllers/users.js
+++ controllers/users.js
@@ -61,15 +61,16 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user.id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
-        })
-        .send({
-          name: user.name, about: user.about, avatar: user.avatar, email: user.email,
-        });
+      // res
+      //   .cookie('jwt', token, {
+      //     maxAge: 3600000 * 24 * 7,
+      //     httpOnly: true,
+      //     sameSite: true,
+      //   })
+      //   .send({
+      //     name: user.name, about: user.about, avatar: user.avatar, email: user.email,
+      //   });
+      res.send({ token });
     })
     .catch((err) => next(err));
 };
